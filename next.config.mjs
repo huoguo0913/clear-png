ee994a38b7537045f8010a25d7afb3a41e1c7b94
wanglei <lei.wang@hangtianyunji.com>
2026-09-16 20:50:57 +0800

    #2 bug修改

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   output: "export",
   poweredByHeader: false,
-  // Force webpack instead of turbopack for static export
-  webpack: (config) => {
-    return config;
-  },
 };
 
 export default nextConfig;
